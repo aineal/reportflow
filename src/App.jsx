f76819efd12829f5e33be70519cf70a1f0d72b21
Reece <kyerafarm@gmail.com>
2026-03-13 08:52:48 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
 import { Toaster } from "@/components/ui/toaster"
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClientInstance } from '@/lib/query-client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
-// Add page imports here
 import Home from "./pages/Home";
 import Report from "./pages/Report";
-import { Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <QueryClientProvider client={queryClientInstance}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Navigate to="/Home" replace />} />
-          <Route path="/Home" element={<Home />} />
-          <Route path="/Report" element={<Report />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/Home" replace />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Report" element={<Report />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
       <Toaster />
-    </QueryClientProvider>
+    </Router>
   );
 }
 
